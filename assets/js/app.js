0668f3aa8d6dcdd166d4f3f22a0bbcb05c7d401e
jony <ravshanovjony7@gmail.com>
2025-05-11 18:01:56 +0500

    main section

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+const header = document.querySelector("header");
+const scrolNavbar = document.querySelector('nav');
+const width = window.getComputedStyle(scrolNavbar).top,
+widthNum = width.slice(0, width.length - 2);
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= 45) {
+        scrolNavbar.classList.add("active-nav")
+    } else {
+        scrolNavbar.classList.remove("active-nav")
+    }
+})
+
+
+
