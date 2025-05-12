673b407beca5b142c2f47bf009f1b30ec05ff34a
jony <ravshanovjony7@gmail.com>
2025-05-12 19:59:01 +0500

    main

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,24 @@
-const header = document.querySelector("header");
-const scrolNavbar = document.querySelector('nav');
-const width = window.getComputedStyle(scrolNavbar).top,
-widthNum = width.slice(0, width.length - 2);
+window.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("header");
+  const scrolNavbar = document.querySelector("nav");
+  const width = window.getComputedStyle(scrolNavbar).top,
+    widthNum = width.slice(0, width.length - 2);
 
-window.addEventListener('scroll', function () {
+  window.addEventListener("scroll", function () {
     if (window.scrollY >= 45) {
-        scrolNavbar.classList.add("active-nav")
+      scrolNavbar.classList.add("active-nav");
     } else {
-        scrolNavbar.classList.remove("active-nav")
+      scrolNavbar.classList.remove("active-nav");
     }
-})
+  });
 
 
 
+
+
+
+
+
+
+  
+});
